e2853cba5fd25c704e8db5bd7f278c0192b70743
bright.lin <bright.lin@qunar.com>
2015-09-08 12:05:23 +0800

    增加功能

--- web/myweb.go
+++ web/myweb.go
@@ -5,14 +5,21 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
-	"math/rand"
 )
+
 type eat_data struct {
-EatRand string
-TotalEat []string
-RandSeed int
+	EatRand  string
+	TotalEat []string
+	RandSeed int
+}
+
+var data = eat_data{
+	"",
+	[]string{"麦当劳", "肯德基", "金渝川菜", "面香八方", "G先生", "西少爷", "蒸功夫"},
+	0,
 }
 
 var addr = flag.String("addr", ":8091", "http service address") // Q=17, R=18
@@ -24,6 +31,7 @@ func main() {
 	flag.Parse()
 	http.Handle("/", http.HandlerFunc(QR))
 	http.Handle("/eat", http.HandlerFunc(EAT))
+	http.Handle("/addEat", http.HandlerFunc(addEat))
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal(`ListenAndServe:`, err)
@@ -48,22 +56,37 @@ func leav(s string) {
 }
 
 func QR(w http.ResponseWriter, req *http.Request) {
-	defer leav(enter(fmt.Sprintf("s=%v", req.FormValue("s")), fmt.Sprintf("req=%v", req.Form.Encode())))
+	defer leav(enter(fmt.Sprintf("s=%v", req.FormValue("s")), fmt.Sprintf("req=%v", req.Form.Encode()), fmt.Sprintf("host=%v", req.RemoteAddr)))
 	templ.Execute(w, req.FormValue("s"))
 }
 
-
-
 func EAT(w http.ResponseWriter, req *http.Request) {
 	defer leav(enter(fmt.Sprintf("req=%v", req.Form.Encode())))
-	data := eat_data{
-		"",
-		[]string{"麦当劳","肯德基","金渝川菜","面向八方","G先生","西少爷","蒸功夫"},
-		0,
-	}
-	data.RandSeed = rand.Int()%len(data.TotalEat)
+
+	data.RandSeed = rand.Int() % len(data.TotalEat)
 	data.EatRand = data.TotalEat[data.RandSeed]
 	templ_eat.Execute(w, data)
+}
+
+func addEat(w http.ResponseWriter, req *http.Request) {
+	defer leav(enter(fmt.Sprintf("req=%v", req.Form.Encode())))
+
+	res := req.FormValue("s")
+	res_add := ""
+	if len(res) <= 0 {
+		res_add = ""
+	} else {
+		for _, value := range data.TotalEat {
+			if value == res {
+				res_add = ""
+				break
+			}
+		}
+	}
+	if len(res_add) > 0 {
+		data.TotalEat = append(data.TotalEat, res)
+	}
+	http.Redirect(w, req, "/eat", 301)
 }
 
 const templateStr = `
@@ -93,6 +116,12 @@ const templatestr_eat = `
 <title>吃什么</title>
 </head>
 <body>
+<form action="/addEat" name=f method="GET">
+<input maxLength=1024 size=70 name=s value="" title="Add Eat">
+<input type=submit value="添加餐馆" name=addEat>
+</form>
+<br>
+<br>
 <br>
 <tr><td>所有可选餐馆</td></tr>
 {{range .TotalEat}}<div>{{ . }}</div>{{else}}<div><strong>no rows</strong></div>{{end}}
